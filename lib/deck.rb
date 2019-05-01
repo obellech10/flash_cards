1a1133c3a5e21918a825d48903469fd38b6d6e45
Oscar Bellechasse <obellech10@gmail.com>
2019-05-01 14:52:58 -0600

    Change cards in category to use each

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,4 +1,5 @@
 require './lib/card'
+require 'pry'
 
 class Deck
   attr_reader :cards
@@ -8,11 +9,22 @@ class Deck
   end
 
   def count
-    cards.count
+    @cards.count
   end
 
+  # def cards_in_category(category)
+  #   cards.select{|card| card.category == category }
+  # end
   def cards_in_category(category)
-    cards.select{|card| card.category == category }
+    card_category = []
+
+    @cards.each do |card|
+      if category == card.category
+        card_category.push(card)
+      end
+    end
+    card_category
   end
+
 
 end
