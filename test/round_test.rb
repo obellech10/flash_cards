cf00711ace886e85acaf1c05c78a64d832c13d04
Oscar Bellechasse <obellech10@gmail.com>
2019-05-03 18:16:24 -0600

    Add test_it_has_feedback method

--- test/round_test.rb
+++ test/round_test.rb
@@ -4,6 +4,7 @@ require '../lib/card'
 require '../lib/deck'
 require '../lib/round'
 require '../lib/turn'
+require 'pry'
 
 class RoundTest < MiniTest::Test
   def setup
@@ -11,28 +12,43 @@ class RoundTest < MiniTest::Test
     @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
     @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     @deck = Deck.new([@card_1, @card_2, @card_3])
-    @round = Round.new(@deck)
+    @round_1 = Round.new(@deck)
     @turn = Turn.new("Juneau", @card_1)
   end
 
   def test_it_exists
-    assert_instance_of Round, @round
+    assert_instance_of Round, @round_1
   end
 
   def test_it_has_a_deck
-    assert_equal @deck, @round.deck
+    assert_equal @deck, @round_1.deck
   end
 
   def test_it_takes_a_turn
-    assert_equal [], @round.turns
+    assert_equal [], @round_1.turns
   end
 
   def test_current_card
-    assert_equal @card_1, @round.current_card
+    assert_equal @card_1, @round_1.current_card
   end
 
   def test_if_it_takes_a_new_turn
-    assert_equal @turn.guess, @round.take_turn("Juneau").guess
+    @round_1.take_turn("Juneau")
+    assert_instance_of Turn, @round_1.turns.first
+    # assert_equal @turn.class, Turn
   end
 
+  def test_if_turn_is_correct
+    @round_1.take_turn("Juneau")
+
+    assert_equal true , @round_1.turns[0].correct?
+    @round_1.take_turn("Alaska")
+    assert_equal false, @round_1.turns[1].correct?
+  end
+
+  def test_it_takes_another_turn
+    skip
+    @round_1.take_turn("Juneau")
+    assert_equal 1, @round_1.turns.count
+  end
 end
