08a3a2cc537caac7281404e9efd5adb760a362ab
Oscar Bellechasse <obellech10@gmail.com>
2019-05-05 11:38:29 -0600

    Change take_turn method

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,6 @@
-require '../lib/card'
-require '../lib/deck'
+require './lib/card'
+require './lib/deck'
+require './lib/turn'
 
 class Round
   attr_reader :deck, :turns
@@ -7,15 +8,26 @@ class Round
   def initialize(deck)
     @deck = deck
     @turns = []
+    @correct = 0
   end
 
   def current_card
     @deck.cards[0]
+    # @deck.cards.shift
   end
 
   def take_turn(guess)
     new_turn = Turn.new(guess, self.current_card)
-    @turns << new_turn
+      @turns << new_turn
+      @turns.first
+
+  end
+
+  def number_correct
+    if @turns.first.correct?
+      @correct += 1
+    end
+    @correct
   end
 
 end
